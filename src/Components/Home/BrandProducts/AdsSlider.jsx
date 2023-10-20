fa503f7c02f58c1af7cae970a3fbceabdf9a22de
tushar-454 <imtushar454@gmail.com>
2023-10-20 16:20:10 +0600

    ads slider image added from local

--- src/Components/Home/BrandProducts/AdsSlider.jsx
+++ src/Components/Home/BrandProducts/AdsSlider.jsx
@@ -2,6 +2,9 @@ import { Component } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
+import slider1 from '../../../assets/slider-1.jpg';
+import slider2 from '../../../assets/slider-2.jpg';
+import slider3 from '../../../assets/slider-3.jpg';
 export default class AdsSlider extends Component {
   render() {
     const settings = {
@@ -17,13 +20,13 @@ export default class AdsSlider extends Component {
       <div>
         <Slider {...settings}>
           <div className='w-full'>
-            <img src='https://cutt.ly/kwQ01rH7' className='w-full rounded-md' />
+            <img src={slider1} className='w-full rounded-md' />
           </div>
           <div className='w-full'>
-            <img src='https://cutt.ly/WwQ009xc' className='w-full rounded-md' />
+            <img src={slider2} className='w-full rounded-md' />
           </div>
           <div className='w-full'>
-            <img src='https://cutt.ly/TwQ09p32' className='w-full rounded-md' />
+            <img src={slider3} className='w-full rounded-md' />
           </div>
         </Slider>
       </div>
