377fb97e0291a8e3bd0bcd43b74a71ce8c0ed26a
tushar-454 <imtushar454@gmail.com>
2023-10-20 16:31:02 +0600

    login and signup illustration update for better look

--- src/Components/UI/FormIllustration.jsx
+++ src/Components/UI/FormIllustration.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const FormIllustrarion = ({ src }) => {
   return (
     <div className='w-full md:w-2/4 hidden md:block'>
-      <img src={src} className='w-full' />
+      <img src={src} className='w-full rounded-md' />
     </div>
   );
 };
